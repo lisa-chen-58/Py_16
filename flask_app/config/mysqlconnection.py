be2aa54eec8d705260bb7aade6d6bf458fcaf8f3
lisa-chen-58 <lisa.hl.chen@gmail.com>
2022-08-29 00:20:58 -0700

    revamp

--- flask_app/config/mysqlconnection.py
+++ flask_app/config/mysqlconnection.py
@@ -3,10 +3,10 @@ import pymysql.cursors
 # this class will give us an instance of a connection to our database
 class MySQLConnection:
     def __init__(self, db):
-        # change the user and password as needed
+    # change the user and password as needed
         connection = pymysql.connect(host = 'localhost',
                                     user = 'root', 
-                                    password = 'Gogopowerrangers9!', 
+                                    password = 'root', 
                                     db = db,
                                     charset = 'utf8mb4',
                                     cursorclass = pymysql.cursors.DictCursor,
@@ -22,15 +22,15 @@ class MySQLConnection:
     
                 cursor.execute(query, data)
                 if query.lower().find("insert") >= 0:
-                    # INSERT queries will return the ID NUMBER of the row inserted
+                # INSERT queries will return the ID NUMBER of the row inserted
                     self.connection.commit()
                     return cursor.lastrowid
                 elif query.lower().find("select") >= 0:
-                    # SELECT queries will return the data from the database as a LIST OF DICTIONARIES
+                # SELECT queries will return the data from the database as a LIST OF DICTIONARIES
                     result = cursor.fetchall()
                     return result
                 else:
-                    # UPDATE and DELETE queries will return nothing
+                # UPDATE and DELETE queries will return nothing
                     self.connection.commit()
             except Exception as e:
                 # if the query fails the method will return FALSE
@@ -42,3 +42,4 @@ class MySQLConnection:
 # connectToMySQL receives the database we're using and uses it to create an instance of MySQLConnection
 def connectToMySQL(db):
     return MySQLConnection(db)
+
